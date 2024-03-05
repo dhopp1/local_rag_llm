31fdce3ab9b17ac70cb7d0102492478fc9715531
dhopp1 <daniel.hopp1@gmail.com>
2024-03-05 15:57:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="local_rag_llm",
+    version="0.0.0",
+    author="Daniel Hopp",
+    author_email="daniel.hopp@un.org",
+    description="Create and run a local LLM with RAG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dhopp1/nowcast_lstm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
