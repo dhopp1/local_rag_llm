5a7bbe444987b2bb1d979fb6c85062a11bdefd35
dhopp1 <daniel.hopp1@gmail.com>
2024-03-06 14:06:26 +0100

    handling different model download scenarios

--- local_rag_llm/model_setup.py
+++ local_rag_llm/model_setup.py
@@ -7,7 +7,7 @@ def instantiate_model(
     text_path,
     llm_url, 
     llm_path,
-    redownload_model,
+    redownload_llm,
     temperature,
     max_new_tokens,
     context_window,
@@ -19,30 +19,33 @@ def instantiate_model(
     if llm_path is None:
         temp_file = tempfile.NamedTemporaryFile()
         llm_path = temp_file.name
-        redownload_model = True
+        redownload_llm = True
     
-    if redownload_model:
+    if redownload_llm:
         print("downloading model...")
         urllib.request.urlretrieve(llm_url, llm_path)
     
     # if no text path given, run vanilla LLM
-    if text_path is None:
-        llm = Llama(
-             model_path = llm_path, 
-             n_ctx = context_window,
-             temperature = temperature,
-             n_gpu_layers = n_gpu_layers
-         )
-    else:
-        llm = LlamaCPP(
-            model_url = llm_url,
-            model_path = llm_path,
-            temperature = temperature,
-            max_new_tokens = max_new_tokens,
-            context_window = context_window,
-            generate_kwargs = {},
-            model_kwargs = {"n_gpu_layers": n_gpu_layers},
-            verbose = True
-        )
+    try:
+        if text_path is None:
+            llm = Llama(
+                 model_path = llm_path, 
+                 n_ctx = context_window,
+                 temperature = temperature,
+                 n_gpu_layers = n_gpu_layers
+             )
+        else:
+            llm = LlamaCPP(
+                model_url = llm_url,
+                model_path = llm_path,
+                temperature = temperature,
+                max_new_tokens = max_new_tokens,
+                context_window = context_window,
+                generate_kwargs = {},
+                model_kwargs = {"n_gpu_layers": n_gpu_layers},
+                verbose = True
+            )
+    except:
+        print("model not found, if passing an llm_path, try setting redownload_llm = True")
         
     return llm
